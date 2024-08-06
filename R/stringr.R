0fd48e0ef301271f7df25fb622b5492818ee5091
Beatriz Milz <milz.bea@gmail.com>
2024-08-06 10:06:52 -0300

    translation - frutas

---
+++ R/stringr.R
@@ -0,0 +1,14 @@
+#' Vetores de caracteres do pacote stringr
+#'
+#' Amostra de vetores de caracteres para praticar a manipulação de strings
+#'
+#' frutas - é uma seleção e adaptação ao português dos dados de prática contidos em stringr::fruit.
+#' @format Um vetor
+#' @name dados-stringr
+#' @examples
+#' length(frutas)
+#' frutas[1:5]
+NULL
+
+#' @rdname dados-stringr
+"frutas"
