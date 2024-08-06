eea3484d536c08b6fd1318dd5455ac7be2b14d44
Beatriz Milz <milz.bea@gmail.com>
2024-08-06 10:34:24 -0300

    palavras #90

--- R/stringr.R
+++ R/stringr.R
@@ -3,12 +3,20 @@
 #' Amostra de vetores de caracteres para praticar a manipulação de strings
 #'
 #' frutas - é uma seleção e adaptação ao português dos dados de prática contidos em stringr::fruit.
+#'
+#' palavras - é uma lista de palavras frequentes usadas em português brasileiro
 #' @format Um vetor
 #' @name dados-stringr
 #' @examples
 #' length(frutas)
 #' frutas[1:5]
+#'
+#' #' length(palavras)
+#' palavras[1:5]
 NULL
 
 #' @rdname dados-stringr
 "frutas"
+
+#' @rdname dados-stringr
+"palavras"
