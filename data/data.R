a6d9acee07a24b722416da806f7074f480154f42
Beatriz Milz <milz.bea@gmail.com>
2026-08-19 08:33:30 -0300

    fix datos > dados

--- data/data.R
+++ data/data.R
@@ -1,88 +1,88 @@
 delayedAssign('companhias_aereas',
-        eval(parse(file.path(system.file('scripts','companhias_aereas.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','companhias_aereas.txt', package = 'dados')))))
 delayedAssign('aeroportos',
-        eval(parse(file.path(system.file('scripts','aeroportos.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','aeroportos.txt', package = 'dados')))))
 delayedAssign('casas',
-        eval(parse(file.path(system.file('scripts','casas.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','casas.txt', package = 'dados')))))
 delayedAssign('dados_atmosfera',
-        eval(parse(file.path(system.file('scripts','dados_atmosfera.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_atmosfera.txt', package = 'dados')))))
 delayedAssign('premios_gerentes',
-        eval(parse(file.path(system.file('scripts','premios_gerentes.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','premios_gerentes.txt', package = 'dados')))))
 delayedAssign('bebes',
-        eval(parse(file.path(system.file('scripts','bebes.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','bebes.txt', package = 'dados')))))
 delayedAssign('rebatedores',
-        eval(parse(file.path(system.file('scripts','rebatedores.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','rebatedores.txt', package = 'dados')))))
 delayedAssign('top100musicas',
-        eval(parse(file.path(system.file('scripts','top100musicas.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','top100musicas.txt', package = 'dados')))))
 delayedAssign('cms_paciente_experiencia',
-        eval(parse(file.path(system.file('scripts','cms_paciente_experiencia.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','cms_paciente_experiencia.txt', package = 'dados')))))
 delayedAssign('comuns',
-        eval(parse(file.path(system.file('scripts','comuns.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','comuns.txt', package = 'dados')))))
 delayedAssign('diamante',
-        eval(parse(file.path(system.file('scripts','diamante.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','diamante.txt', package = 'dados')))))
 delayedAssign('velho_fiel',
-        eval(parse(file.path(system.file('scripts','velho_fiel.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','velho_fiel.txt', package = 'dados')))))
 delayedAssign('jardineiros',
-        eval(parse(file.path(system.file('scripts','jardineiros.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','jardineiros.txt', package = 'dados')))))
 delayedAssign('voos',
-        eval(parse(file.path(system.file('scripts','voos.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','voos.txt', package = 'dados')))))
 delayedAssign('dados_gapminder',
-        eval(parse(file.path(system.file('scripts','dados_gapminder.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_gapminder.txt', package = 'dados')))))
 delayedAssign('questionario',
-        eval(parse(file.path(system.file('scripts','questionario.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','questionario.txt', package = 'dados')))))
 delayedAssign('nucleo_familiar',
-        eval(parse(file.path(system.file('scripts','nucleo_familiar.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','nucleo_familiar.txt', package = 'dados')))))
 delayedAssign('dados_iris',
-        eval(parse(file.path(system.file('scripts','dados_iris.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_iris.txt', package = 'dados')))))
 delayedAssign('gerentes',
-        eval(parse(file.path(system.file('scripts','gerentes.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','gerentes.txt', package = 'dados')))))
 delayedAssign('milhas',
-        eval(parse(file.path(system.file('scripts','milhas.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','milhas.txt', package = 'dados')))))
 delayedAssign('mtcarros',
-        eval(parse(file.path(system.file('scripts','mtcarros.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','mtcarros.txt', package = 'dados')))))
 delayedAssign('pinguins',
-        eval(parse(file.path(system.file('scripts','pinguins.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pinguins.txt', package = 'dados')))))
 delayedAssign('pessoas',
-        eval(parse(file.path(system.file('scripts','pessoas.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pessoas.txt', package = 'dados')))))
 delayedAssign('arremessadores',
-        eval(parse(file.path(system.file('scripts','arremessadores.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','arremessadores.txt', package = 'dados')))))
 delayedAssign('pixar_oscars',
-        eval(parse(file.path(system.file('scripts','pixar_oscars.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_oscars.txt', package = 'dados')))))
 delayedAssign('pixar_bilheteria',
-        eval(parse(file.path(system.file('scripts','pixar_bilheteria.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_bilheteria.txt', package = 'dados')))))
 delayedAssign('pixar_filmes',
-        eval(parse(file.path(system.file('scripts','pixar_filmes.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_filmes.txt', package = 'dados')))))
 delayedAssign('pixar_generos',
-        eval(parse(file.path(system.file('scripts','pixar_generos.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_generos.txt', package = 'dados')))))
 delayedAssign('pixar_equipe',
-        eval(parse(file.path(system.file('scripts','pixar_equipe.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_equipe.txt', package = 'dados')))))
 delayedAssign('pixar_avalicao_publico',
-        eval(parse(file.path(system.file('scripts','pixar_avalicao_publico.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','pixar_avalicao_publico.txt', package = 'dados')))))
 delayedAssign('avioes',
-        eval(parse(file.path(system.file('scripts','avioes.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','avioes.txt', package = 'dados')))))
 delayedAssign('presidentes_eua',
-        eval(parse(file.path(system.file('scripts','presidentes_eua.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','presidentes_eua.txt', package = 'dados')))))
 delayedAssign('salarios',
-        eval(parse(file.path(system.file('scripts','salarios.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','salarios.txt', package = 'dados')))))
 delayedAssign('dados_starwars',
-        eval(parse(file.path(system.file('scripts','dados_starwars.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_starwars.txt', package = 'dados')))))
 delayedAssign('tabela1',
-        eval(parse(file.path(system.file('scripts','tabela1.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela1.txt', package = 'dados')))))
 delayedAssign('tabela2',
-        eval(parse(file.path(system.file('scripts','tabela2.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela2.txt', package = 'dados')))))
 delayedAssign('tabela3',
-        eval(parse(file.path(system.file('scripts','tabela3.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela3.txt', package = 'dados')))))
 delayedAssign('tabela4a',
-        eval(parse(file.path(system.file('scripts','tabela4a.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela4a.txt', package = 'dados')))))
 delayedAssign('tabela4b',
-        eval(parse(file.path(system.file('scripts','tabela4b.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela4b.txt', package = 'dados')))))
 delayedAssign('tabela5',
-        eval(parse(file.path(system.file('scripts','tabela5.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','tabela5.txt', package = 'dados')))))
 delayedAssign('veiculos',
-        eval(parse(file.path(system.file('scripts','veiculos.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','veiculos.txt', package = 'dados')))))
 delayedAssign('clima',
-        eval(parse(file.path(system.file('scripts','clima.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','clima.txt', package = 'dados')))))
 delayedAssign('dados_oms',
-        eval(parse(file.path(system.file('scripts','dados_oms.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_oms.txt', package = 'dados')))))
 delayedAssign('dados_oms2',
-        eval(parse(file.path(system.file('scripts','dados_oms2.txt', package = 'datos')))))
+        eval(parse(file.path(system.file('scripts','dados_oms2.txt', package = 'dados')))))
